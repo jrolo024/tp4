4dd467452f813e8da94b337d44243b865117810d
jrolo024 <54976521+jrolo024@users.noreply.github.com>
2020-04-01 12:28:16 +0200

    Update tp4.js

--- tp4.js
+++ tp4.js
@@ -112,13 +112,140 @@ function TestAplatissement(){
 	console.log( flatten2D([[1,2], [3,4], [5,6]]));   // [1, 2, 3, 4, 5, 6]
 	console.log( flatten2D([[1,2], [3,4,5], [6]]));   // [1, 2, 3, 4, 5, 6]
 }
+//ex6
+function ex6(){
+    let tab = ["Bob", "Julien", "Roger"];
+    console.log(tab.join(", "));
+    tab.pop();
+    tab.push("jess");
+}
+//ex7
+function multiple(){
+    let tab = [];
+    tab.push([" ",1,2,3,4,5,6,7,8,9]);
+    for(let i = 1; i < 10; i++){
+        let ligne = [];
+        ligne.push(i);
+        for(let j =1; j < 10; j++){
+            ligne.push(i*j);
+        }
+        tab.push(ligne);
+    }
+    console.log(tab.join("\n"));
+}
+//ex8
+function chiffrementCesar(message, decalage){
+    let resultat = "";
+    for(let i = 0; i < message.length; i++){
 
+        if(message[i] != " "){
+
+            if ((message.charCodeAt(i) >= 65) && (message.charCodeAt(i) <= 90)){
+
+				resultat += String.fromCharCode((message.charCodeAt(i)-65+decalage)%26+65);
+				
+            }else if ((message.charCodeAt(i) >= 97) && (message.charCodeAt(i) <= 122)){
+
+				resultat += String.fromCharCode((message.charCodeAt(i)-97+decalage)%26+97);
+				
+            }
+        }else{
+            resultat += " ";
+        }
+	}
+	
+    return resultat;
+}
+
+function dechiffrementCesar(message, decalage){
+    let result = "";
+    for(let i = 0; i < message.length; i++){
+        if(message[i] != " "){
+            if ((message.charCodeAt(i) >= 65) && (message.charCodeAt(i) <= 90)){
+                let dec = message.charCodeAt(i)-65-decalage;
+                if(dec<0){
+                    dec+=26;
+                }
+                result += String.fromCharCode(dec%26+65);
+            }else if ((message.charCodeAt(i) >= 97) && (message.charCodeAt(i) <= 122)){
+                let dec = message.charCodeAt(i)-97-decalage;
+                if(dec<0){
+                    dec+=26;
+                }
+                result += String.fromCharCode(dec%26+97);
+            }
+        }else{
+            result += " ";
+        }
+    }
+    return result;
+}
+//ex9
+function chiffrementViginere(message, decalage){
+    let result = "";
+    let tabCle = [];
+    while(decalage >= 1){
+        tabCle.unshift(Math.trunc(decalage%10));
+        decalage = Math.trunc(decalage/10);
+    }
+    let posCle = 0;
+    for(let i = 0; i < message.length; i++){
+        if(message[i] != " "){
+            if ((message.charCodeAt(i) >= 65) && (message.charCodeAt(i) <= 90)){
+                result += String.fromCharCode((message.charCodeAt(i)-65+tabCle[posCle])%26+65);
+            }else if ((message.charCodeAt(i) >= 97) && (message.charCodeAt(i) <= 122)){
+                result += String.fromCharCode((message.charCodeAt(i)-97+tabCle[posCle])%26+97);
+            }
+            posCle++;
+            if(posCle >= tabCle.length){
+                posCle = 0;
+            }
+        }else{
+            result += " ";
+        }
+    }
+    return result;
+}
+
+function dechiffrementViginere(message, decalage){
+    let result = "";
+    let tabCle = [];
+    while(decalage >= 1){
+        tabCle.unshift(Math.trunc(decalage%10));
+        decalage = Math.trunc(decalage/10);
+    }
+    let posCle = 0;
+    for(let i = 0; i < message.length; i++){
+        if(message[i] != " "){
+            if ((message.charCodeAt(i) >= 65) && (message.charCodeAt(i) <= 90)){
+                let dec = message.charCodeAt(i)-65-tabCle[posCle];
+                if(dec<0){
+                    dec+=26;
+                }
+                result += String.fromCharCode(dec%26+65);
+            }else if ((message.charCodeAt(i) >= 97) && (message.charCodeAt(i) <= 122)){
+                let dec = message.charCodeAt(i)-97-tabCle[posCle];
+                if(dec<0){
+                    dec+=26;
+                }
+                result += String.fromCharCode(dec%26+97);
+            }
+            posCle++;
+            if(posCle >= tabCle.length){
+                posCle = 0;
+            }
+        }else{
+            result += " ";
+        }
+    }
+    return result;
+}
 function main(){
 	//Testsum();
 	//TestWord ();
 	//TestRange ();
 	//TestOccurence ();
-	TestAplatissement ();
+	//éTestAplatissement ();
 }
 
 main();
